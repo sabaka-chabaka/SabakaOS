8d1d058ffd1a27e9ccf73d639d681a822a7073c2
sokor <gamekimg360@gmail.com>
2026-04-02 17:42:40 +0400

    justify kernel

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -38,52 +38,6 @@ static void vga_fill(char c, int row, unsigned char fg, unsigned char bg=0) {
 #define USER_CODE_VIRT 0x20000000u
 #define USER_CODE_SIZE 4096u
 
-/*__attribute__((section(".user_text"), noinline))
-static void user_hello_proc() {
-    __asm__ volatile(
-        "jmp skip_data\n"
-        "msg_ok:  .ascii \"[BRK TEST] Memory allocated and writable!\\n\"\n"
-        "msg_err: .ascii \"[BRK TEST] Failed to allocate memory...\\n\"\n"
-        "skip_data:\n"
-
-        "mov $45, %%eax\n"
-        "xor %%ebx, %%ebx\n"
-        "int $0x80\n"
-        "mov %%eax, %%esi\n"
-
-        "add $4096, %%eax\n"
-        "mov %%eax, %%ebx\n"
-        "mov $45, %%eax\n"
-        "int $0x80\n"
-
-        "cmp %%esi, %%eax\n"
-        "je fail\n"
-
-        "movb $'!', (%%esi)\n"
-
-        "mov $4, %%eax\n"
-        "mov $1, %%ebx\n"
-        "lea msg_ok, %%ecx\n"
-        "mov $38, %%edx\n"
-        "int $0x80\n"
-        "jmp exit\n"
-
-    "fail:\n"
-        "mov $4, %%eax\n"
-        "mov $1, %%ebx\n"
-        "lea msg_err, %%ecx\n"
-        "mov $35, %%edx\n"
-        "int $0x80\n"
-
-    "exit:\n"
-        "mov $1, %%eax\n"
-        "xor %%ebx, %%ebx\n"
-        "int $0x80\n"
-        ::: "eax", "ebx", "ecx", "edx", "esi"
-    );
-}*/
-
-
 static Mutex shared_mutex;
 static volatile uint32_t shared_counter = 0;
 
@@ -107,37 +61,11 @@ static void mutex_proc_b(void*) {
     process_exit();
 }
 
-/*static void pipe_producer(void* arg) {
-    int pipe_id = (int)(uintptr_t)arg;
-    const char* msgs[] = { "hello", "from", "pipe", nullptr };
-    for (int i = 0; msgs[i]; i++) {
-        pipe_write(pipe_id, msgs[i], kstrlen(msgs[i]));
-        pipe_write(pipe_id, "\n", 1);
-        process_sleep(100);
-    }
-    pipe_close_write(pipe_id);
-    process_exit();
-}
-
-static void pipe_consumer(void* arg) {
-    int pipe_id = (int)(uintptr_t)arg;
-    char buf[64];
-    int n;
-    while ((n = pipe_read(pipe_id, buf, 63)) > 0) {
-        buf[n] = 0;
-        for (int i = 0; i < n; i++)
-            terminal_putchar(buf[i]);
-    }
-    pipe_destroy(pipe_id);
-    process_exit();
-}*/
-
 extern "C" void kernel_main() {
     for(int i=0;i<80*25;i++) VGA[i]=ve(' ',15,0);
 
     vga_fill(' ', 0, 15, 1);
-    vga_print("  SabakaOS v0.2.0", 0, 0, 15, 1);
-    vga_print("[x86 | Ring3 | Syscall | Mutex | Pipe]", 0, 33, 14, 1);
+    vga_print("  SabakaOS v0.3.0 x86", 0, 0, 15, 1);
 
     gdt_init();
     idt_init();
@@ -191,34 +119,10 @@ extern "C" void kernel_main() {
     process_create(mutex_proc_a, nullptr, "mutex_a",   5);
     process_create(mutex_proc_b, nullptr, "mutex_b",   5);
 
-    int pipe_id = pipe_create();
-    if (pipe_id >= 0) {
-        //process_create(pipe_producer, (void*)(uintptr_t)pipe_id, "pipe_prod", 5);
-        //process_create(pipe_consumer, (void*)(uintptr_t)pipe_id, "pipe_cons", 5);
-    }
-
-    // if (paging_alloc_region(USER_CODE_VIRT, USER_CODE_SIZE,
-    //                         PAGE_PRESENT | PAGE_WRITE | PAGE_USER)) {
-    //     uint8_t* dst = (uint8_t*)USER_CODE_VIRT;
-    //     uint8_t* src = (uint8_t*)(uintptr_t)user_hello_proc;
-    //     for (uint32_t i = 0; i < USER_CODE_SIZE; i++)
-    //         dst[i] = src[i];
-    //
-    //     process_create_user(USER_CODE_VIRT, "user_hello", 5);
-    // }
-
-    vga_fill('-', 1, 8);
-
     terminal_init();
     shell_init();
     terminal_set_execute_cb(shell_execute);
     keyboard_set_callback(terminal_on_key);
-
-    terminal_set_color_fg(11);
-    terminal_puts("SabakaOS v0.3.0\n");
-    terminal_reset_color();
-    terminal_puts("Persistent files | Elf executable\n");
-    terminal_puts("type 'ps' to watch processes, 'help' for commands\n\n");
 
     terminal_reply_input();
 
